a0a9aa8b96672c2ec75d8c4395485f23eca8c041
Milos V <milos.v@cmpse.com>
2022-04-06 12:17:09 +0200

    Create user controller for verification and handler route

--- src/entities/user/user.module.ts
+++ src/entities/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 
 @Module({
   imports: [],
+  controllers: [UserController],
   providers: [UserResolver, UserService],
 })
 export class UserModule {}
