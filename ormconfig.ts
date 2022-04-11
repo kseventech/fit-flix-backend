c4369f9d9d7d79c130627b96db87041f28b33c11
Milos V <milos.v@cmpse.com>
2022-04-11 11:30:34 +0200

    Create ormconfig

---
+++ ormconfig.ts
@@ -0,0 +1,22 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import dotenv from 'dotenv';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+dotenv.config();
+
+const ORMConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  entities: ['dist/src/**/*.entity.js'],
+  synchronize: false,
+  migrations: ['dist/src/db/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/db/migrations',
+  },
+  namingStrategy: new SnakeNamingStrategy(),
+};
+export = ORMConfig;
