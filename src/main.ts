0ffebd6d576a77baa792299323eb23a7e5009af1
Milos V <milos.v@cmpse.com>
2022-04-21 16:27:14 +0200

    Import validation pipes in main script

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import { validationPipe } from './common/pipes';
+import { limitValidationPipe, pageValidationPipe, uuidValidationPipe, validationPipe } from './common/pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(process.env.PORT || 3000);
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-  app.useGlobalPipes(validationPipe);
+  app.useGlobalPipes(validationPipe, uuidValidationPipe, pageValidationPipe, limitValidationPipe);
   app.enableCors();
 }
 bootstrap();
