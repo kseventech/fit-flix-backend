25e903a7e52e93256f9736363355d5c8ec089634
Milos V <milos.v@cmpse.com>
2022-04-21 16:26:04 +0200

    Create admin guard and strategy for firebase auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user/entity/user.entity';
 import { UserStrategy } from './strategy/user.strategy';
+import { AdminStrategy } from './strategy/admin.strategy';
 
 @Module({
   imports: [PassportModule, TypeOrmModule.forFeature([User])],
-  providers: [FirebaseStrategy, UserStrategy],
-  exports: [FirebaseStrategy, UserStrategy],
+  providers: [FirebaseStrategy, UserStrategy, AdminStrategy],
+  exports: [FirebaseStrategy, UserStrategy, AdminStrategy],
 })
 export class AuthModule {}
--- src/auth/strategy/admin.strategy.ts
+++ src/auth/strategy/admin.strategy.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt } from 'passport-jwt';
@@ -7,7 +7,7 @@ import { FirebaseAuthStrategy } from 'src/services/firebase/firebase.strategy';
 import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserStrategy extends PassportStrategy(FirebaseAuthStrategy, 'user') {
+export class AdminStrategy extends PassportStrategy(FirebaseAuthStrategy, 'admin') {
   constructor(@InjectRepository(User) private userRepo: Repository<User>) {
     super({
       extractor: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -16,6 +16,7 @@ export class UserStrategy extends PassportStrategy(FirebaseAuthStrategy, 'user')
 
   override async validate(payload: any) {
     if (!payload.email_verified) throw new BadRequestException('Email not verified');
+    if (!payload.admin) throw new ForbiddenException();
     const user = await this.userRepo.findOne({ firebaseId: payload.uid });
     if (!user) throw new NotFoundException('user not found');
     return user;
