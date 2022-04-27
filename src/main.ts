4cf0212095ccaaf695d16da9563f020d725ed583
Milos V <milos.v@cmpse.com>
2022-04-27 14:16:27 +0200

    Exclude validation pipes from global instance

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
-import { limitValidationPipe, pageValidationPipe, uuidValidationPipe, validationPipe } from './common/pipes';
+import { validationPipe } from './common/pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(process.env.PORT || 3000);
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
-  app.useGlobalPipes(validationPipe, uuidValidationPipe, pageValidationPipe, limitValidationPipe);
+  app.useGlobalPipes(validationPipe);
   app.enableCors();
 }
 bootstrap();
