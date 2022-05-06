bdeee8dbad3e944cb46a79285a86b4ec3e859c11
Milos V <milos.v@cmpse.com>
2022-05-06 12:14:01 +0200

    Create decoded firebase user interface

---
+++ src/common/interface/firebase-decoded-user.inteface.ts
@@ -0,0 +1,25 @@
+export class IFirebaseDecodedUser {
+  iss: string;
+
+  aud: string;
+
+  auth_time: number;
+
+  user_id: string;
+
+  sub: string;
+
+  iat: number;
+
+  exp: number;
+
+  email: string;
+
+  email_verified: boolean;
+
+  firebase: any;
+
+  uid: string;
+
+  role: string;
+}
